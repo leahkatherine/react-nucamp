aa5b8a4c168896e787b567a120e202ff8f439378
Leah Anestos <leahkanestos@gmail.com>
2023-07-01 20:48:16 -0400

    card, campsites lists, and footer & header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+    return (
+        <div className="App">
+          <Navbar dark color='primary' sticky='top' expand='md'>
+              <Container>
+                  <NavbarBrand href='/'>
+                      <img src={NucampLogo} alt='nucamp logo'/>
+                  </NavbarBrand>
+              </Container>
+          </Navbar>
+          <CampsitesList />
+          <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,1 @@
+import {Navbar, NavbarBrand} from 'reactstrap';
