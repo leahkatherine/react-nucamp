5ee25f19c5f8dc55bc35d9e574f7e6a7061b1cb4
Leah Anestos <leahkanestos@gmail.com>
2023-07-06 19:47:07 -0400

    Buidiing the nav bar - I moved it from App.js to Header.js, and set the card up how to demo suggests. Now currently adding some boot strap nav bar components for margins, and collapse

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
@@ -11,13 +12,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-          <Navbar dark color='primary' sticky='top' expand='md'>
-              <Container>
-                  <NavbarBrand href='/'>
-                      <img src={NucampLogo} alt='nucamp logo'/>
-                  </NavbarBrand>
-              </Container>
-          </Navbar>
+          <Header/>
           <Routes>
                 <Route path='/'  element={<HomePage/>} />
                 <Route path='contact' element={<ContactPage/>}/>
