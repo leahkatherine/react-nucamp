cb136cf803107112f7b37db8e5cf1b1edd2c1891
Leah Anestos <leahkanestos@gmail.com>
2023-07-06 16:48:30 -0400

    we provded te use of state hooks, hile also rendering the page with cards to be controlled by on click, to display different Campsites for our page

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -3,6 +3,11 @@ import { CAMPSITES } from '../../app/shared/CAMPSITES';
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+//This code is just kept for an example for your future use 
+//export const selectRandomCampsite = () => {
+//    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+//};
+
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
