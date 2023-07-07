7a694b22a6a389922e1e4b2a346f3bb06b51e2cc
Leah Anestos <leahkanestos@gmail.com>
2023-07-07 12:30:11 -0400

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NucampLogo from './app/assets/img/logo.png';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 function App() {
@@ -17,6 +18,10 @@ function App() {
                 <Route path='/'  element={<HomePage/>} />
                 <Route path='contact' element={<ContactPage/>}/>
                 <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+                <Route 
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage/>}
+                />
           </Routes>
           <Footer/>
         </div>
